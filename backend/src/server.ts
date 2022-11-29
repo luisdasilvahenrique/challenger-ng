2cc57a17dbf8b6f01ac84d988464757fb2e0681d
luisdasilvahenrique <luisenriklh1177027@gmail.com>
2022-11-28 22:03:32 -0300

    nothings

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Hello Lola!')
+   res.send('Olá')
 })
+
 
 app.listen(3333);
